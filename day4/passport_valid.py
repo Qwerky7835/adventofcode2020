7debc8f3316ed874c894cd9e0e8f9045f3f16e10
TC1211 <tianchansa@gmail.com>
2021-06-20 21:53:48 +0200

    aoc day 4 complete

--- day4/passport_valid.py
+++ day4/passport_valid.py
@@ -7,17 +7,18 @@ def validate(code, stri):
 	if code == 'byr':
 		if stri.isdigit():
 			intyear = int(stri)
-			if intyear <= 2002 or intyear >= 1920:
+			if intyear <= 2002 and intyear >= 1920:
 				return True
 	if code == 'iyr':
 		if stri.isdigit():
 			intyear = int(stri)
-			if intyear <= 2020 or intyear >= 2010:
+			if intyear <= 2020 and intyear >= 2010:
 				return True
 	if code == 'eyr':
 		if stri.isdigit():
 			intyear = int(stri)
-			if intyear <= 2030 or intyear >= 2020:
+			print(intyear)
+			if intyear < 2031 and intyear > 2019:
 				return True
 	if code == 'hgt':
 		if 'cm' in stri:
@@ -31,7 +32,11 @@ def validate(code, stri):
 				if intheight <= 76 or intheight >= 59:
 					return True
 	if code == 'hcl':
-		if stri[0] == '#' and (all(c in string.hexdigits for c in stri)):
+		if stri[0] == '#':
+			hexcode = stri[1:7]
+			for digit in hexcode:
+				if digit not in "0123456789abcdef":
+					return Falsep
 			return True
 	if code == 'ecl':
 		if len(stri) == 3 and (stri in 'amb blu brn gry grn hzl oth'):
@@ -39,6 +44,8 @@ def validate(code, stri):
 	if code == 'pid':
 		if len(stri) == 9 and stri.isdigit():
 			return True
+	if code == 'cid':
+		return True
 	return False
 
 
@@ -47,7 +54,6 @@ with open("input.txt") as file:
 		if line[0] == "\n":
 			if (len(fields) == 0) or (len(fields)==1 and fields[0] == 'cid'):
 				counter += 1
-				print "here"
 			fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
 		else:
 			listfields = line.split()
@@ -59,8 +65,8 @@ with open("input.txt") as file:
 				else:
 					break
 
-if (len(fields) == 0) or (len(fields)==1 and fields[0] == 'cid'):
-				counter += 1
+# if (len(fields) == 0) or (len(fields)==1 and fields[0] == 'cid'):
+# 				counter += 1
 				
 print counter 
 
